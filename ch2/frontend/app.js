6d3cf143de680454d1162c64ba57c4296c779103
nicolas <41stni1mst@hft-stuttgart.de>
2025-07-08 17:47:01 +0200

    Initial Commit

--- ch2/frontend/app.js
+++ ch2/frontend/app.js
@@ -1,22 +1,18 @@
-var express = require('express');
-var app = express();
-var stringReplace = require('string-replace-middleware');
+const express = require('express');
+const app = express();
+const stringReplace = require('string-replace-middleware');
+const path = require('path');
 
-var KC_URL = process.env.KC_URL || "http://localhost:8080";
-var SERVICE_URL = process.env.SERVICE_URL || "http://localhost:3000/secured";
+const KC_URL = process.env.KC_URL || "http://localhost:8080";
+const SERVICE_URL = process.env.SERVICE_URL || "http://localhost:3000/secured";
 
 app.use(stringReplace({
    'SERVICE_URL': SERVICE_URL,
    'KC_URL': KC_URL
 }));
-app.use(express.static('.'))
 
-app.get('/', function(req, res) {
-    res.render('index.html');
+app.use(express.static('.'));
+
+app.listen(8000, () => {
+    console.log('App is listening on http://localhost:8000');
 });
-
-app.get('/client.js', function(req, res) {
-    res.render('client.js');
-});
-
-app.listen(8000);
